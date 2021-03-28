b95ed86a553eb8d64153693adf672319dca93875
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2021-03-28 10:15:38 +0200

    New Website

---
+++ assets/js/typewriter-custom.js
@@ -0,0 +1,9 @@
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+  strings: ['Amazon Web Service', 'Digital Ocean', 'Google Cloud Platform', 'Vultr', 'Linode', 'Microsoft Azure', 'any Cloud VPS'],
+  autoStart: true,
+  loop: true,
+});
+
+
